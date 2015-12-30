c8d0a718841e6ba17511efe2853f8d0d71f413a1
PHAM NGOC QUYNH <phamngocquynh.mail@gmail.com>
2015-12-30 22:40:24 +0700

    Complete demo Stackviews

---
+++ stackatada-stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackatada-stack
+//
+//  Created by Pham ngoc Quynh on 12/30/15.
+//  Copyright © 2015 Pham ngoc Quynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
